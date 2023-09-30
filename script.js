d4b5bd949d61f39325d637a99f193be248bd9bbe
Gayathri Ramesh <gayathriramesh021@gmail.com>
2023-09-30 10:39:42 +0530

    madrasters

---
+++ script.js
@@ -0,0 +1,127 @@
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+// Team member drag and slideshow
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  } else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
+
+const autoPlay = () => {
+  if (window.innerWidth < 200 || !isAutoPlay) return;
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 1500);
+};
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+
+// Onscroll header bg change
+const navbar = document.querySelector("nav");
+function changeBg() {
+  var scrollValue = window.scrollY;
+  if (scrollValue < 150) {
+    navbar.classList.remove("navBgColor");
+  } else {
+    navbar.classList.add("navBgColor");
+  }
+}
+window.addEventListener("scroll", changeBg);
+
+// Sidebar show
+const menu = document.querySelector(".hamburger");
+const ul = document.querySelector(".nav-items ul");
+
+menu.addEventListener("click", function () {
+  ul.classList.toggle("show");
+});
+
+
+// on scroll nav change
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("#navbar .nav-items li a");
+
+window.onscroll = () => {
+  const scrollPosition = window.scrollY;
+
+  sections.forEach((sec) => {
+    const offset =
+      sec.offsetTop - document.querySelector("#navbar").offsetHeight;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+
+    if (scrollPosition >= offset && scrollPosition < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      const correspondingLink = document.querySelector(
+        "#navbar .nav-items li a[href='#" + id + "']"
+      );
+
+      if (correspondingLink) {
+        correspondingLink.classList.add("active");
+      }
+    }
+  });
+};
